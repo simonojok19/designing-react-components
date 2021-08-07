c2a387873a111bb1355d1ad9d037c7672249d657
simonojok19 <simonojok19@gmail.com>
2021-08-07 09:50:49 +0300

    formated the speaker

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -29,7 +29,14 @@ const SpeakerImage = ({ id, first, last }) => {
   );
 };
 
-const SpeakerDemographics = ({ first, last, bio, twitterHandle, favorite }) => {
+const SpeakerDemographics = ({
+  first,
+  last,
+  bio,
+  twitterHandle,
+  favorite,
+  company,
+}) => {
   return (
     <div className="speaker-info">
       <div className="d-flex justify-content-between mb-3">
@@ -38,9 +45,17 @@ const SpeakerDemographics = ({ first, last, bio, twitterHandle, favorite }) => {
         </h3>
       </div>
       <div>
-        <p>
-          {bio} {twitterHandle} {favorite}
-        </p>
+        <p className="card-description">{bio}</p>
+        <div className="social d-flex flex-row mt-4">
+          <div className="company">
+            <h5>Company</h5>
+            <h6>{company}</h6>
+          </div>
+          <div className="twitter">
+            <h5>Twitter</h5>
+            <h6>{twitterHandle}</h6>
+          </div>
+        </div>
       </div>
     </div>
   );
