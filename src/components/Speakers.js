60081dcdb6927974de35019168e62fc17fa006dc
simonojok19 <simonojok19@gmail.com>
2021-08-07 10:09:57 +0300

    added the page header

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { data } from "../../SpeakerData";
 import SpeakersList from "./SpeakersList";
+import Header from "./Header";
+
 export default function Speakers() {
   return (
     <div className="container-fluid">
+      <Header />
       <SpeakersList data={data} />
     </div>
   );
