1726d1c7bb9bddb28b1bd6fc8d08b7c254d9fd85
simonojok19 <simonojok19@gmail.com>
2021-08-08 20:22:11 +0300

    cleaned the app and using context api

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import SpeakersToolbar from "./SpeakersToolbar";
 import SpeakersList from "./SpeakersList";
 
-export default function Speakers({ theme, setTheme }) {
+export default function Speakers() {
   const [showSessions, setShowSessions] = useState(true);
   return (
     <>
       <SpeakersToolbar
-        theme={theme}
-        setTheme={setTheme}
         showSessions={showSessions}
         setShowSessions={setShowSessions}
       />
