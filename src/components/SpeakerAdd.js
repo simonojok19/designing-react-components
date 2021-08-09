b9899a199266e4a97f9ea5e1f51eb36286bfa981
simonojok19 <simonojok19@gmail.com>
2021-08-09 22:54:04 +0300

    fixed a bug for adding speakers

--- src/components/SpeakerAdd.js
+++ src/components/SpeakerAdd.js
@@ -19,9 +19,9 @@ export default function SpeakerAdd({ eventYear, insertRecord }) {
                 room: {
                   name: "Main Ball Room",
                 },
+                eventYear,
               },
             ],
-            eventYear,
           });
         }}
       >
