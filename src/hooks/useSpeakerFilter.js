48fef7c66afba3c583b93dfbadba7d1fe480d0c6
simonojok19 <simonojok19@gmail.com>
2021-08-09 20:30:38 +0300

    added more event years

--- src/hooks/useSpeakerFilter.js
+++ src/hooks/useSpeakerFilter.js
@@ -1,6 +1,33 @@
 import { useState } from "react";
 
-export default function useSpeakerFilter(startingShowSessions) {
+export default function useSpeakerFilter(
+  startingShowSessions,
+  startingEventYear
+) {
   const [showSessions, setShowSessions] = useState(startingShowSessions);
-  return { showSessions, setShowSessions };
+  const [eventYear, setEventYear] = useState(startingEventYear);
+  const [searchQuery, setSearchQuery] = useState("");
+  const EVENT_YEAR = [
+    "2008",
+    "2009",
+    "2010",
+    "2011",
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+  ];
+  return {
+    showSessions,
+    setShowSessions,
+    eventYear,
+    setEventYear,
+    searchQuery,
+    setSearchQuery,
+    EVENT_YEAR,
+  };
 }
