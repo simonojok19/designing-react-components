a89b5730b424ba09cdc3cd57d850361e05621c5f
simonojok19 <simonojok19@gmail.com>
2021-08-10 08:00:25 +0300

    json error

--- pages/api/speakers/index.js
+++ pages/api/speakers/index.js
@@ -1,4 +1,13 @@
 import { data } from "../../../SpeakerData";
+import path from "path";
+import fs from "fs";
+
+const { promisify } = require("util");
+const { readFile } = promisify(fs.readFile);
+const delay = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 
 export default async function handle(req, res) {
   res.status(200).send(JSON.stringify(data, null, 2));
