93d61c9dd7e2b5aa74179431e737a32f917d145e
simonojok19 <simonojok19@gmail.com>
2021-08-10 20:33:30 +0300

    added get request

--- pages/api/speakers/index.js
+++ pages/api/speakers/index.js
@@ -10,5 +10,19 @@ const delay = (ms) =>
   });
 
 export default async function handle(req, res) {
-  res.status(200).send(JSON.stringify(data, null, 2));
+  // res.status(200).send(JSON.stringify(data, null, 2));
+  const jsonFile = path.resolve("./", "db.json");
+  try {
+    const readFileData = await readFile(jsonFile);
+    await delay(1000);
+    const speakers = JSON.parse(readFileData).speakers;
+    if (speakers) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(speakers, null, 2));
+      console.log("GET /api/speakers status: 200");
+    }
+  } catch (e) {
+    console.log("/api/speakers error", e);
+    res.status(404).send("File Not Found on Server");
+  }
 }
